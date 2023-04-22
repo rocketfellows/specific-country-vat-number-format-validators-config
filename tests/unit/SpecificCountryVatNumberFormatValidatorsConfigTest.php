e7cf1622d9fd84bcf145d2a4b0fe74265a3be6a0
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-04-22 15:22:28 +0400

    [CVFV-28] started implementing.

--- tests/unit/SpecificCountryVatNumberFormatValidatorsConfigTest.php
+++ tests/unit/SpecificCountryVatNumberFormatValidatorsConfigTest.php
@@ -2,9 +2,13 @@
 
 namespace rocketfellows\SpecificCountryVatNumberFormatValidatorsConfig\tests\unit;
 
+use arslanimamutdinov\ISOStandard3166\ISO3166;
 use PHPUnit\Framework\TestCase;
+use rocketfellows\CountryVatFormatValidatorInterface\CountryVatFormatValidatorInterface;
+use rocketfellows\CountryVatFormatValidatorInterface\CountryVatFormatValidators;
 use rocketfellows\CountryVatNumberFormatValidatorsConfig\CountryVatNumberFormatValidatorsConfigInterface;
 use rocketfellows\SpecificCountryVatNumberFormatValidatorsConfig\tests\unit\mocks\configs\RUVatNumberFormatValidatorsConfig;
+use rocketfellows\SpecificCountryVatNumberFormatValidatorsConfig\tests\unit\mocks\validators\RUVatFormatValidator;
 
 class SpecificCountryVatNumberFormatValidatorsConfigTest extends TestCase
 {
@@ -13,5 +17,44 @@ class SpecificCountryVatNumberFormatValidatorsConfigTest extends TestCase
     public function testSpecificConfigImplementedInterface(): void
     {
         $this->assertEquals(self::EXPECTED_INTERFACE_IMPLEMENTATION, (new RUVatNumberFormatValidatorsConfig()));
+    }
+
+    /**
+     * @param CountryVatFormatValidatorInterface|null $defaultValidator
+     * @param CountryVatFormatValidators|null $additionalValidators
+     * @param CountryVatFormatValidatorInterface[] $expectedCountryValidators
+     */
+    public function testInitializeSpecificConfigByDifferentUseCases(
+        ?CountryVatFormatValidatorInterface $defaultValidator,
+        ?CountryVatFormatValidators $additionalValidators,
+        array $expectedCountryValidators
+    ): void {
+        $config = new RUVatNumberFormatValidatorsConfig($defaultValidator, $additionalValidators);
+
+        $actualValidators = [];
+
+        foreach ($config->getValidators() as $actualValidator) {
+            $actualValidators[] = $actualValidator;
+        }
+
+        $this->assertEquals(ISO3166::RU(), $config->getCountry());
+        $this->assertEquals($actualValidators, $expectedCountryValidators);
+    }
+
+    public function getSpecificConfigValidators(): array
+    {
+        $firstAdditionalValidator = $this->createMock(CountryVatFormatValidatorInterface::class);
+        $secondAdditionalValidator = $this->createMock(CountryVatFormatValidatorInterface::class);
+        $thirdAdditionalValidator = $this->createMock(CountryVatFormatValidatorInterface::class);
+
+        return [
+            'default validator not set and additional validators not set' => [
+                'defaultValidator' => null,
+                'additionalValidators' => null,
+                'expectedCountryValidators' => [
+                    (new RUVatFormatValidator()),
+                ],
+            ],
+        ];
     }
 }
