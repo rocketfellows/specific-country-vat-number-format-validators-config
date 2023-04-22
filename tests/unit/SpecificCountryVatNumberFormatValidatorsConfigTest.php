247a82c932e50847a2b32b01c061b0e79e006a3f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-04-22 15:07:38 +0400

    [CVFV-28] covered specific config implemented interface.

--- tests/unit/SpecificCountryVatNumberFormatValidatorsConfigTest.php
+++ tests/unit/SpecificCountryVatNumberFormatValidatorsConfigTest.php
@@ -3,7 +3,15 @@
 namespace rocketfellows\SpecificCountryVatNumberFormatValidatorsConfig\tests\unit;
 
 use PHPUnit\Framework\TestCase;
+use rocketfellows\CountryVatNumberFormatValidatorsConfig\CountryVatNumberFormatValidatorsConfigInterface;
+use rocketfellows\SpecificCountryVatNumberFormatValidatorsConfig\tests\unit\mocks\configs\RUVatNumberFormatValidatorsConfig;
 
 class SpecificCountryVatNumberFormatValidatorsConfigTest extends TestCase
 {
+    private const EXPECTED_INTERFACE_IMPLEMENTATION = CountryVatNumberFormatValidatorsConfigInterface::class;
+
+    public function testSpecificConfigImplementedInterface(): void
+    {
+        $this->assertEquals(self::EXPECTED_INTERFACE_IMPLEMENTATION, (new RUVatNumberFormatValidatorsConfig()));
+    }
 }
